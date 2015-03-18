40b5c20ff19653134a35dd0535f7c748ac2599b0
ariel-algo <Gershengold.Ariel.Algo@gmail.com>
2015-03-18 12:24:43 +0200

    fix the week candels

--- MT4Processor/src/com/algotrado/data/event/basic/japanese/JapaneseTimeFrameType.java
+++ MT4Processor/src/com/algotrado/data/event/basic/japanese/JapaneseTimeFrameType.java
@@ -7,14 +7,15 @@ import java.util.GregorianCalendar;
 
 
 public enum JapaneseTimeFrameType {
+	
 	ONE_MINUTE(1,"1 minute"),
 	FIVE_MINUTE(5,"5 minute"),
 	FIFTEEN_MINUTES(15,"15 minute"),
 	THIRTY_MINUTES(30,"30 minute"),
 	ONE_HOUR(60,"1 hour"),
-	FOUR_HOURS(240,"4 hours"),
-	ONE_DAY(1440,"day"),
-	ONE_WEEK(10080,"week")/*,
+	FOUR_HOURS(60*4,"4 hours"),
+	ONE_DAY(60*24,"day"),
+	ONE_WEEK(60*24*7,"week")/*,
 	ONE_MONTH(40320)*/;//Comment: Month period is counted as 4 weeks and may not be accurate. Maybe we should not support month for now.
 	
 	public String getValueString() {
@@ -22,6 +23,7 @@ public enum JapaneseTimeFrameType {
 	}
 	private static final long MINUTES_IN_MILISEC = 60*1000;
 	private static final long DAY_IN_MINUTES = 24*60;
+	private static final long WEEK_IN_MINUTES = 24*60*7;
 	private int valueInMinutes;
 	private String valueString;
 	private long timeOffsetFromZeroDateInMinutes;
@@ -30,7 +32,7 @@ public enum JapaneseTimeFrameType {
 	private JapaneseTimeFrameType(int valueInMinutes,String valueString){
 		this.valueInMinutes = valueInMinutes;
 		this.valueString = valueString;
-		if(valueInMinutes == 10080) // one week - TODO, check way we can't use this == ONE_WEEK ???
+		if(valueInMinutes == WEEK_IN_MINUTES) // one week - TODO, check way we can't use this == ONE_WEEK ???
 		{
 			Date tempDate = new Date(0);
 			Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
@@ -53,8 +55,8 @@ public enum JapaneseTimeFrameType {
 	
 	public Date getRoundDate(Date date)
 	{
-		long dateInMinutes = date.getTime()/MINUTES_IN_MILISEC;
-		long newDateInMinutes = ((long)dateInMinutes/valueInMinutes) * valueInMinutes;
+		long dateInMinutes = date.getTime()/MINUTES_IN_MILISEC +  timeOffsetFromZeroDateInMinutes;
+		long newDateInMinutes = ((long)dateInMinutes/valueInMinutes) * valueInMinutes - timeOffsetFromZeroDateInMinutes;
 		Date newDate = new Date(newDateInMinutes*MINUTES_IN_MILISEC);
 		return newDate;
 	}
@@ -111,18 +113,7 @@ public enum JapaneseTimeFrameType {
 	 * @return
 	 */
 	public boolean isTimeFrameEndTime(Date time, int lowerInterval) {
-//		Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
-//		calendar.setTime(time);
-//		long currTimeInMinutes;
-//    	if (valueInMinutes <= 60) {
-//    		currTimeInMinutes = calendar.get(Calendar.MINUTE);
-//    	} else if (valueInMinutes <= 1440) {
-//    		currTimeInMinutes = (calendar.get(Calendar.HOUR_OF_DAY) * 60) + calendar.get(Calendar.MINUTE);
-//    	} else { //week timeframe
-//    		currTimeInMinutes = (calendar.get(Calendar.DAY_OF_WEEK) * 1440) + (calendar.get(Calendar.HOUR_OF_DAY) * 60) + calendar.get(Calendar.MINUTE);
-//    	}
-//      return (currTimeInMinutes / lowerInterval) % (valueInMinutes/ lowerInterval) == ((valueInMinutes/ lowerInterval) - 1);
-		long currTimeInMinutes = time.getTime()/MINUTES_IN_MILISEC - timeOffsetFromZeroDateInMinutes;
+		long currTimeInMinutes = time.getTime()/MINUTES_IN_MILISEC + timeOffsetFromZeroDateInMinutes;
 		return (currTimeInMinutes + lowerInterval) % valueInMinutes == 0;
 	}
 	
@@ -132,32 +123,8 @@ public enum JapaneseTimeFrameType {
 	 * @return
 	 */
 	public boolean isTimeFrameStartTime(Date time, Date openTime) {
-//		Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
-//    	calendar.setTime(time);
-//    	long currTimeInMinutes;
-//    	if (valueInMinutes <= 60) {
-//    		currTimeInMinutes = calendar.get(Calendar.MINUTE);
-//    	} else if (valueInMinutes <= 1440) {
-//    		currTimeInMinutes = (calendar.get(Calendar.HOUR_OF_DAY) * 60) + calendar.get(Calendar.MINUTE);
-//    	} else { //week timeframe
-//    		currTimeInMinutes = (calendar.get(Calendar.DAY_OF_WEEK) * 1440) + (calendar.get(Calendar.HOUR_OF_DAY) * 60) + calendar.get(Calendar.MINUTE);
-//    	}
-//
-//    	Calendar calendarPrev = GregorianCalendar.getInstance(); // creates a new calendar instance
-//    	calendarPrev.setTime(prevStartTime);
-//    	long prevTimeInMinutes;
-//    	if (valueInMinutes <= 60) {
-//    		prevTimeInMinutes = calendarPrev.get(Calendar.MINUTE);
-//    	} else if (valueInMinutes <= 1440) {
-//    		prevTimeInMinutes = (calendarPrev.get(Calendar.HOUR_OF_DAY) * 60) + calendarPrev.get(Calendar.MINUTE);
-//    	} else { //week timeframe
-//    		prevTimeInMinutes = (calendarPrev.get(Calendar.DAY_OF_WEEK) * 1440) + (calendarPrev.get(Calendar.HOUR_OF_DAY) * 60) + calendarPrev.get(Calendar.MINUTE);
-//    	}
-//        return time.after(prevStartTime) && 
-//        		( ((currTimeInMinutes % valueInMinutes) <= (prevTimeInMinutes % valueInMinutes))
-//        		 || ((time.getTime() - prevStartTime.getTime())/(60000)) > valueInMinutes);
-		long openTimeInMinutes = (openTime.getTime() / MINUTES_IN_MILISEC) - timeOffsetFromZeroDateInMinutes; // in JapaneseCandleDataExtractor the prevStartTime is Floor to interval
-		long timeInMinutesFloorInMinutes = (time.getTime()/ MINUTES_IN_MILISEC) - timeOffsetFromZeroDateInMinutes;
+		long openTimeInMinutes = (openTime.getTime() / MINUTES_IN_MILISEC) + timeOffsetFromZeroDateInMinutes; // in JapaneseCandleDataExtractor the prevStartTime is Floor to interval
+		long timeInMinutesFloorInMinutes = (time.getTime()/ MINUTES_IN_MILISEC) + timeOffsetFromZeroDateInMinutes;
 		return (openTimeInMinutes/valueInMinutes) < (timeInMinutesFloorInMinutes/valueInMinutes);
 	}
 }
