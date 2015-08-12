a681e1fb4960e2df5d4ed50373571d5af8f98abf
Ohad Behore <ohadbehore@gmail.com>
2015-08-12 21:17:20 +0300

    Get History from IB. This version works well. Although after 2 days therewas aprobewith IB gateway reonneting to the broker.

--- MT4Processor/src/com/algotrado/interactive/brokers/tws/TestIbDataExtract.java
+++ MT4Processor/src/com/algotrado/interactive/brokers/tws/TestIbDataExtract.java
@@ -6,11 +6,13 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.SwingUtilities;
 
 import apidemo.util.NewTabbedPanel.NewTabPanel;
 
+import com.algotrado.util.Setting;
 import com.ib.client.EClientErrors;
 import com.ib.controller.ApiConnection.ILogger;
 import com.ib.controller.ApiController;
@@ -26,7 +28,7 @@ import com.ib.controller.Types.WhatToShow;
 
 public class TestIbDataExtract implements IConnectionHandler, Runnable {
 	
-	private static final int THOUSAND_SECONDS = 1000000;
+	private static final int THOUSAND_EIGHT_HUNDRED_SECONDS = 1800000;
 	private final ILogger m_inLogger = new LoggerForTws( );
 	private final ILogger m_outLogger = new LoggerForTws( );
 	private ApiController m_controller = new ApiControllerWrapper(this, m_inLogger, m_outLogger, "C:\\Algo\\Asset History Date\\QM\\oil_action_log_" + System.currentTimeMillis() + ".txt", true);
@@ -56,10 +58,7 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 		
 		this.qmContract = qmContract;
 		
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-		}
+		threadSleep(5000);
 	}
 	
 	public ApiController controller() 		{ return m_controller; }
@@ -93,18 +92,18 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 		//	qmContract.primaryExch("ISLAND");
 		qmContract.secType(SecType.FUT);
 		qmContract.symbol("QM");
-		qmContract.localSymbol("QMQ5");
+		qmContract.localSymbol("QMU5");
 		qmContract.tradingClass("QM");
 		qmContract.expiry("20150819 12:00:00");
 		qmContract.multiplier("500");
 		
 		
 		historyDataRecorder = new TestIbDataExtract(qmContract);
-		historyDataRecorder.initialEndDateFormatted = "20150101 00:00:00";
+		historyDataRecorder.initialEndDateFormatted = "20150725 00:15:00";
 		try {
 			historyDataRecorder.initialEndDate = Bar.FORMAT.parse(historyDataRecorder.initialEndDateFormatted);
 		} catch (ParseException e1) {
-			System.out.println("Error parsing initial date.");
+			Setting.errShow("Error parsing initial date.");
 			e1.printStackTrace();
 			return;
 		}
@@ -118,46 +117,56 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 
 	private void historyRequestsRunnable(NewContract qmContract) {
 		while (initialEndDate.before(new Date())) {
+			long startTime = System.currentTimeMillis();
 	//		String genericTickList = "";
 			
 			
 	//		boolean snapshot = false;
 	//		test.controller().reqTopMktData(contract, genericTickList, snapshot , test.getUpdatedAssetData());
+			threadSleep(1);
 			if (isConnected) {
 			
-				this.controller().reqHistoricalData(qmContract, initialEndDateFormatted, 1000, DurationUnit.SECOND, BarSize._1_secs, WhatToShow.TRADES, false, this.historyBarModel);
+				this.controller().reqHistoricalData(qmContract, initialEndDateFormatted, 1800, DurationUnit.SECOND, BarSize._1_secs, WhatToShow.TRADES, false, this.historyBarModel);
 			
 				try {
-					this.getSemaphore().acquire();
-					Thread.sleep(10000);
+					if (this.getSemaphore().tryAcquire(1, TimeUnit.SECONDS)) {
+						
+					} else {
+						
+					}
 				} catch (InterruptedException e) {
-					System.out.println("Get History From Broker Process was interrupted.");
+					show("Get History From Broker Process was interrupted.");
 					e.printStackTrace();
 				}//Block until request finishes.
 				
-				if (isConnected) {
-					this.initialEndDate = new Date(this.initialEndDate.getTime() + THOUSAND_SECONDS);
+				threadSleep(10001 - (System.currentTimeMillis()-startTime));
+				
+//				if (isConnected) {
+					this.initialEndDate = new Date(this.initialEndDate.getTime() + THOUSAND_EIGHT_HUNDRED_SECONDS);
 				
 	//			test.getCalendar().setTime(initialEndDate);
 					
 					initialEndDateFormatted = Bar.FORMAT.format(initialEndDate);
-				}
+//				}
 			}
 			
 			
 		}
 		this.controller().disconnect();
 		
+		Setting.outShow("Finished get history");
+		
 		try {
 			this.historyBarModel.getDestinationFile().close();
 		} catch (IOException e1) {
-			System.out.println("Could not close file" + this.historyBarModel.getDestinationFile().toString());
+			show("Could not close file" + this.historyBarModel.getDestinationFile().toString());
 			e1.printStackTrace();
 		}
 	}
 	
 	static class BarResultsPanel extends NewTabPanel implements IHistoricalDataHandler, IRealTimeBarHandler {
-//		final BarModel m_model = new BarModel();
+private static final int ONE_DAY_MILLIS = 24*60*60*1000;
+		//		final BarModel m_model = new BarModel();
 		final ArrayList<Bar> m_rows = new ArrayList<Bar>();
 //		final boolean m_historical;
 //		final Chart m_chart = new Chart( m_rows);
@@ -173,7 +182,7 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 			try {
 				this.destinationFile = new FileWriter(filePath, false);
 			} catch (IOException e) {
-				System.err.println("Exception occoured while trying to open file for writing.");
+				Setting.errShow("Exception occoured while trying to open file for writing.");
 				e.printStackTrace();
 			}
 		}
@@ -205,22 +214,22 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if ((initialEndDateParsed.getTime() % 86400000) < (THOUSAND_SECONDS + 1)) {
-				System.err.println("New Day Initial Formatted End Date=" + historyDataRecorder.initialEndDateFormatted);
+			if ((initialEndDateParsed.getTime() % ONE_DAY_MILLIS) < (THOUSAND_EIGHT_HUNDRED_SECONDS + 1)) {
+				Setting.errShow("New Day Initial Formatted End Date=" + historyDataRecorder.initialEndDateFormatted);
 			}
 				
-			if (initialEndDateParsed.getTime() > (barDate.getTime() + THOUSAND_SECONDS + 1) /*||
+			if (initialEndDateParsed.getTime() > (barDate.getTime() + THOUSAND_EIGHT_HUNDRED_SECONDS + 1) /*||
 					historyDataRecorder.initialEndDate.getTime() > (barDate.getTime() + THOUSAND_SECONDS + 1)*/) {
-				System.err.println("1. Bar 					 Date=" + bar.formattedTime());
-				System.err.println("2. Initial 			 End Date=" + historyDataRecorder.initialEndDate);
-				System.err.println("3. Initial Formatted End Date=" + historyDataRecorder.initialEndDateFormatted);
-			} else if ((prevBar == null) || (bar.close() != prevBar.close()) ||
+				Setting.errShow("1. Bar 					 Date=" + bar.formattedTime());
+				Setting.errShow("2. Initial 			 End Date=" + historyDataRecorder.initialEndDate);
+				Setting.errShow("3. Initial Formatted End Date=" + historyDataRecorder.initialEndDateFormatted);
+			} else if (/*(prevBar == null) || (bar.close() != prevBar.close()) ||
 						(bar.open() != prevBar.open()) ||
 						(bar.high() != prevBar.high()) ||
-						(bar.low() != prevBar.low()) || 
+						(bar.low() != prevBar.low()) ||*/ 
 						(bar.volume() > 0)) {
 //				if (prevBar == null) {
-					prevBar = bar;
+//					prevBar = bar;
 //				}
 				m_rows.add(bar);
 			}
@@ -236,7 +245,7 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 				destinationFile.flush();
 				this.m_rows.clear();
 			} catch (IOException e) {
-				System.out.println("Error trying to append data to file." + this.getClass());
+				Setting.outShow("Error trying to append data to file." + this.getClass());
 				e.printStackTrace();
 			}
 			semaphore.release();
@@ -252,7 +261,7 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 
 		@Override
 		public void log(String valueOf) {
-			System.err.println(valueOf);
+			Setting.errShow(valueOf);
 		}
 		
 	}
@@ -261,6 +270,10 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 	public void connected() {
 		show( "connected 123");
 		isConnected = true;
+		releaseSemaphore();
+	}
+
+	private void releaseSemaphore() {
 		if (this.getSemaphore().availablePermits() <= 0) {
 			this.getSemaphore().release();
 		}
@@ -290,27 +303,31 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 	public void message(int id, int errorCode, String errorMsg) {
 		// Handle errors:
 		handleErrors(errorCode);
-		show( id + " " + errorCode + " " + errorMsg);
+		show("ID: "+ id + " Error Code: " + errorCode + " Error msg: " + errorMsg);
 	}
 
 	private void handleErrors(int errorCode) {
 		switch(errorCode) {
 		case EClientErrors.CONNECT_FAILED_CONST: //
-			System.out.println("Could not connect to TWS, Check that it is working.");
+			Setting.errShow("Could not connect to TWS, Check that it is working.");
+			restartHistoryRecorder();
 		break;
 		case 1: 
 		break;
 		case 2108://EClientErrors.NOT_CONNECTED_CONST:
+			restartHistoryRecorder();
+			break;
 		case 162://162 Historical Market Data Service error message:HMDS query returned no data: QMU5@NYMEX Trades
 			// move date forward and try extracting more data.
 //			TestIbDataExtract.getHistoryDataRecorder().initialEndDate = 
-//				new Date(TestIbDataExtract.getHistoryDataRecorder().initialEndDate.getTime() + 1000000);
-			restartHistoryRecorder();
+//				new Date(TestIbDataExtract.getHistoryDataRecorder().initialEndDate.getTime() + 1800000);
+			Setting.errShow("Error 162, date tried to withdraw:" + historyDataRecorder.initialEndDateFormatted);
+//			restartHistoryRecorder();
 			break;
 		case 2103: //Market data farm connection is broken, Disconnect and connect again.
 			restartHistoryRecorder();
 			break;
-		case 1100:
+		case 1100://Connectivity between IB and TWS BeTrader has been lost.
 		case 2110:
 			isConnected = false;
 			break;
@@ -324,18 +341,24 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 	}
 
 	private void restartHistoryRecorder() {
+		isConnected = false;
 		TestIbDataExtract.getHistoryDataRecorder().controller().disconnect();
 		long startTimestamp = System.currentTimeMillis();
-		while (System.currentTimeMillis() < (startTimestamp + 10000)) {
+		threadSleep(startTimestamp);
+//		connectAPIController();
+//		new Thread(TestIbDataExtract.getHistoryDataRecorder()).start();
+		
+	}
+
+	private void threadSleep(long howLongToWait) {
+		long startTimestamp = System.currentTimeMillis();
+		while ((System.currentTimeMillis() - startTimestamp) < howLongToWait) {
 			try {
-				Thread.sleep(10000);
+				Thread.sleep(howLongToWait - (System.currentTimeMillis() - startTimestamp));
 			} catch (InterruptedException e) {
 //				e.printStackTrace();
 			}
 		}
-//		connectAPIController();
-//		new Thread(TestIbDataExtract.getHistoryDataRecorder()).start();
-		
 	}
 
 	private void connectAPIController() {
@@ -345,7 +368,7 @@ public class TestIbDataExtract implements IConnectionHandler, Runnable {
 
 	@Override
 	public void show(String string) {
-		System.err.print(string);
+		Setting.outShow(string);
 	}
 
 //	public Calendar getCalendar() {
@@ -363,15 +386,16 @@ public void run() {
 //	try {
 //		initialEndDate = Bar.FORMAT.parse(initialEndDateFormatted);
 //	} catch (ParseException e1) {
-//		System.out.println("Error parsing initial date.");
+//		Setting.outShow("Error parsing initial date.");
 //		e1.printStackTrace();
 //		return;
 //	}
 	
 	try 
 	{
-		// Thread.sleep (1000);
-		while (! (TestIbDataExtract.getHistoryDataRecorder().controller().getClientConnection().isConnected()));
+		while (! (TestIbDataExtract.getHistoryDataRecorder().controller().getClientConnection().isConnected())){
+			Thread.sleep (1);
+		}
 	} catch (Exception e) 
 	{
 	};
--- MT4Processor/src/com/algotrado/util/Setting.java
+++ MT4Processor/src/com/algotrado/util/Setting.java
@@ -12,6 +12,7 @@ public class Setting {
 	private static double usOilBrokerSpread = 0.05;
 	private static double usOilTopSpread = 0.01;
 	private static double usOilBottomSpread = 0.01;
+	private static boolean needToPrint = true;
 	
 	
 	public static String getDateTimeFormat(Date date) {
@@ -44,4 +45,16 @@ public class Setting {
 		return usOilBottomSpread;
 	}
 	
+	public static void errShow(String err)
+	{
+		if(needToPrint)
+			System.err.println(err);
+	}
+	
+	
+	public static void outShow(String out)
+	{
+		if(needToPrint)
+			System.out.println(out);
+	}
 }
